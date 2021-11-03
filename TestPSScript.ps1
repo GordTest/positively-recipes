5e73e43f8ea0bbe7baca3fe08b032b0236532abb
Gordon Packer <gordon@gordonpacker.co.uk>
2021-11-03 10:52:02 +0000

    second commit

---
+++ TestPSScript.ps1
@@ -0,0 +1,4 @@
+﻿#This is just a test script and doesn't really do anything!
+# Well ok... it creates a messagebox that says it was shared on GitHub! ;-)
+
+[System.Windows.MessageBox]::Show('I was shared on GitHub!')
